710ec7f27d715e708048bf400c1891e03398357d
jacgit18 <jacdevgoo2020@gmail.com>
2021-09-04 20:14:54 -0400

    add test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+// import Highcharts from './highcharts';
+// import HighchartsReact from './highcharts-react-official';
+import ReactHighcharts from "react-highcharts";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const config = {
+  chart: {
+    type: 'bar'
+},
+title: {
+    text: 'Fruit Consumption'
+},
+xAxis: {
+    categories: ['Apples', 'Bananas', 'Oranges']
+},
+yAxis: {
+    title: {
+        text: 'Fruit eaten'
+    }
+},
+series: [{
+    name: 'Jane',
+    data: [1, 0, 4]
+}, {
+    name: 'John',
+    data: [5, 7, 3]
+}]
+
+};
+
+
+class App extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+
+    return (
+      <div className="App ">
+            <ReactHighcharts config={config} ref="chart" />
+            <ReactHighcharts config={config} ref="chart" />
+
+
+      
+      </div>
+      
+
+    )}
+
+
 }
+
+// Optional after-render callback
+// const afterRender = (chart) => { /* do stuff with the chart  */ };
+// <ReactHighcharts config = {config} callback = {afterRender}></ReactHighcharts>
 
 export default App;
